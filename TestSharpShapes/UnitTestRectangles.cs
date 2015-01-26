fe7a41861051c8dfa8fbc0b1bc1162fb7f0045c3
beck410 <beck410@hotmail.com>
2015-01-26 11:23:35 -0600

    added rectangle class and constructor

---
+++ TestSharpShapes/UnitTestRectangles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sharpshapes;
+
+namespace TestSharpShapes {
+  [TestClass]
+  public class UnitTestRectangles {
+    [TestMethod]
+    public void TestRectangleConstructor() {
+      Rectangle rectangle = new Rectangle(40, 50);
+      Assert.AreEqual(40, rectangle.Width);
+      Assert.AreEqual(50, rectangle.Height);
+    }
+
+    [TestMethod]
+    [ExpectedException (typeof(ArgumentException))]
+    
+    public void TestRectangleConstructorSanityCheckWidth() {
+      Rectangle rectangle = new Rectangle(0, 50);
+    }
+
+    [TestMethod]
+    [ExpectedException (typeof(ArgumentException))]
+    
+    public void TestRectangleConstructorSanityCheckHeight() {
+      Rectangle rectangle = new Rectangle(50,0);
+    }
+
+    [TestMethod]
+    [ExpectedException (typeof(ArgumentException))]
+    
+    public void TestRectangleConstructorSanityChecksHieghtPositivity() {
+      Rectangle rectangle = new Rectangle(50, -1);
+    }
+
+
+  }
+}
---
+++ sharpshapes/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sharpshapes {
+  public class Rectangle {
+
+    private int width;
+    public object Width {
+      get { return this.width; } 
+    }
+
+    private int height;
+    public object Height {
+      get { return this.height; } 
+    }
+
+    public Rectangle(int width, int height) {
+
+      if(width <= 0 || height <=0)
+        throw new ArgumentException();
+
+      this.width = width;
+      this.height = height;
+    }
+
+
+
+  }
+}
