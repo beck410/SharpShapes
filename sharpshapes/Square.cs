9c11d7def555e393f02e61691bd4911acf96c77b
beck410 <beck410@hotmail.com>
2015-01-26 14:46:09 -0600

    refactored square class

--- sharpshapes/Square.cs
+++ sharpshapes/Square.cs
@@ -4,44 +4,49 @@ using System.Linq;
 using System.Text;
 
 namespace sharpshapes {
-  public class Square : Shape{
+  public class Square : Rectangle{
+
+    public Square(int size) : base(size, size)
+    {
   
-    private  decimal width;
-    public decimal Width {
-      get { return this.width; } 
     }
+  
+  //  private  decimal width;
+  //  public decimal Width {
+  //    get { return this.width; } 
+  //  }
 
-    private  decimal height;
-    public decimal Height {
-      get { return this.height; } 
-    }
+  //  private  decimal height;
+  //  public decimal Height {
+  //    get { return this.height; } 
+  //  }
     
-    public Square(int size) {
-      if (size <= 0)
-        throw new ArgumentException();
+  //  public Square(int size) {
+  //    if (size <= 0)
+  //      throw new ArgumentException();
 
-      this.width = size;
-      this.height = size;
-    }
+  //    this.width = size;
+  //    this.height = size;
+  //  }
 
-    public override void Scale(int percent) {
-      if (percent <= 0)
-        throw new ArgumentException();
+  //  public override void Scale(int percent) {
+  //    if (percent <= 0)
+  //      throw new ArgumentException();
 
-      this.width = width * percent / 100;
-      this.height = height * percent / 100;
-    }
+  //    this.width = width * percent / 100;
+  //    this.height = height * percent / 100;
+  //  }
 
-    public override int SidesCount {
-      get { return 4; }
-    }
+  //  public override int SidesCount {
+  //    get { return 4; }
+  //  }
 
-    public override decimal Area() {
-      return Width * Height;
-    }
+  //  public override decimal Area() {
+  //    return Width * Height;
+  //  }
 
-    public override decimal Perimeter() {
-      return (2 * Width) + (2 * Height);
-    }
-  }
+  //  public override decimal Perimeter() {
+  //    return (2 * Width) + (2 * Height);
+  //  }
+ }
 }
